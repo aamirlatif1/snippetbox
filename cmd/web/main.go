4416b55dca3f6832b63309e7b375c1821b15d50a
Aamir Latif <aamirlatif@gmail.com>
2022-03-13 17:57:19 +0100

    Improved error handling and routing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	addr := flag.String("addr", ":4040", "HTTP network address")
+	flag.Parse()
 
-	log.Println("Starting server on :4040")
-	err := http.ListenAndServe(":4040", mux)
-	log.Fatal(err)
+	app := application{
+		errorLog: log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+	}
+
+	app.infoLog.Printf("Starting server on %s", *addr)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: app.errorLog,
+		Handler:  app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	app.errorLog.Fatal(err)
 }
